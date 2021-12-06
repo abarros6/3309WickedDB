7fbca41f31bea44ae32e733b56bb849d4358e831
Anthony Barros <abarros6@uwo.ca>
2021-12-06 15:41:15 -0500

    re-organize project

---
+++ assignment4/sql copy/Functions.sql
@@ -0,0 +1,46 @@
+--given student name find their info
+SELECT* 
+FROM Student
+WHERE fName=//given AND lName= //given;
+
+--find the lowest salaried staff from each department
+SELECT d.deptName, a.adminNo, min(a.salary), i.instructorNo, min(i.salary) 
+FROM Department as d Right JOIN AdminStaff as a  RIGHT JOIN Instructor as i
+GROUP BY d.deptName;
+
+--given a student and a course find all students in the same section of that course with them
+SELECT s.fName, s.lName 
+FROM Enrollment e, Student s 
+
+WHERE e.sectionID= (
+    SELECT e.sectionID
+    FROM Enrollment e, Section s 
+    WHERE e.studentNo=//given AND s.courseID= //given
+    )
+  And e.studentNo = s.studentNo ;
+
+--add a new student to the student relation
+INSERT INTO Student
+VALUES (//allgiven);
+--note: the interface should have a selector list for calssroom number and instructor number to make sure they are valid
+
+--increase the slaries of all instructors by a selected percest
+UPDATE Instructor
+SET salary = salary* //given%
+WHERE deptName = given;
+
+--look up a student name and find all the courses they are taking
+--note: this function is highly ineficient
+SELECT t.courseID, e.startTime, e.endTime
+FROM Enrollment e, Student s, Section t
+WHERE s.fName = //given AND s.lName = //given AND s.studentNo = e.studentNo AND e.sectionID = t.sectionID;
+
+
+
+
+
+
+
+
+
+
---
+++ assignment4/sql copy/ex2.sql
@@ -0,0 +1,130 @@
+
+CREATE TABLE Department(
+    deptName varchar(255),
+    instructorsAssiged int,
+    studentsEnrolled int,
+    PRIMARY KEY (deptName)
+);
+--removed instructor no as instructor already holds a reference to classNo
+CREATE TABLE Classroom(
+    classroomNo int,
+    classroomQuantity int,
+    --number of courses tought
+    coursesTaught int,
+    PRIMARY KEY (classroomNo)
+);
+
+CREATE TABLE AdminStaff(
+    adminNo int NOT NULL AUTO_INCREMENT,
+    position varchar(255),
+    startDate DATE,
+    deptName varchar(255),
+    fName varchar(255),
+    lName varchar(255),
+    salary int,
+    PRIMARY KEY (adminNo),
+    FOREIGN KEY (deptName) REFERENCES Department(deptName)
+    ON DELETE SET NULL ON UPDATE CASCADE  
+);
+
+--added reference to department
+CREATE TABLE Instructor(
+    instructorNo int NOT NULL AUTO_INCREMENT,
+    classroomNo int,
+    deptName varchar(255),
+    coursesTaught int,
+    fName varchar(255),
+    lName varchar(255),
+    salary int,
+    PRIMARY KEY (instructorNo),
+    FOREIGN KEY (classroomNo) REFERENCES Classroom(classroomNo)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (deptName) REFERENCES Department(deptName)
+    ON DELETE SET NULL ON UPDATE CASCADE  
+);
+
+CREATE TABLE Student(
+    studentNo int NOT NULL AUTO_INCREMENT,
+    fName varchar(255),
+    lName varchar(255),
+    studentAge int,
+    studentYear int,
+    studentAverage int,
+    creditsToDate int,
+    numberOfClasses int,
+    classroomNo int,
+    instructorNo int,
+    PRIMARY KEY (studentNo),
+    FOREIGN KEY (classroomNo) REFERENCES Classroom(classroomNo)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (instructorNo) REFERENCES Instructor(instructorNo)
+    ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+
+
+CREATE TABLE Course(
+    courseID int,
+    courseName varchar(255),
+    courseClassroom int,
+    deptName varchar(255),
+    courseYear int,
+    instructorNo int,
+    PRIMARY KEY (courseID),
+    FOREIGN KEY (courseClassroom) REFERENCES Classroom(classroomNo)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (deptName) REFERENCES Department(deptName)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (instructorNo) REFERENCES Instructor(instructorNo)
+    ON DELETE SET NULL ON UPDATE CASCADE
+);
+--changed timing name to section, added id with auto increment
+CREATE TABLE Section(
+    sectionID int NOT NULL AUTO_INCREMENT,
+    courseID int,
+    startDate DATE,
+    endDate DATE,
+    startTime TIME,
+    endTime TIME,
+    PRIMARY KEY (sectionID),
+    FOREIGN KEY (courseID) REFERENCES Course(courseID)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+);
+--enrollment now references a section 
+CREATE TABLE Enrollment(
+    studentNo int,
+    sectionID int,
+    dateEnrolled DATE,
+
+    PRIMARY KEY (studentNo,sectionID),
+    FOREIGN KEY (studentNo) REFERENCES Student(studentNo)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (sectionID) REFERENCES Section(sectionID)
+	ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE CourseEquipment(
+    deviceNo int NOT NULL AUTO_INCREMENT,
+    deviceName varchar(255),
+    courseAssigned int,
+    studentNo int,
+    dueDate DATE,
+    PRIMARY KEY (deviceNo),
+    FOREIGN KEY (courseAssigned) REFERENCES Course(courseID)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (studentNo) REFERENCES Student(studentNo)
+    ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Allotment(
+    instructorNo int,
+    classroomNo int,
+    dateAssigned DATE,
+    timeAssigned TIME,
+    PRIMARY KEY (instructorNo,classroomNo),
+    FOREIGN KEY (instructorNo) REFERENCES Instructor(instructorNo)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (classroomNo) REFERENCES Classroom(classroomNo)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
