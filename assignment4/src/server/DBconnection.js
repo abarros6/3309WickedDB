60458d96990089f77c279a1729a3afbab5260e19
Ahilltob <ahilltob@uwo.ca>
2021-12-06 22:58:55 -0500

    function sql updated

--- assignment4/src/server/DBconnection.js
+++ assignment4/src/server/DBconnection.js
@@ -5,7 +5,7 @@ function newConnection()
     let conn = mysql.createConnection({
         host:'127.0.0.1',
         user: 'root',
-        password:'6474584026Ab',
+        password:'',
         database:'wicked',
         port: '3306',
         multipleStatements: true,
